69c358d0b93e2aac520681aabdc91fa82b996428
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-05-26 15:40:35 -0300

    Delete MemoBio directory

--- MemoBio/cods/jsPagUsua.js
+++
@@ -1,6 +0,0 @@
-
-const menu = document.querySelector("#btnMenu")
-menu.addEventListener("click", () => {
-	const header = document.querySelector(".menuUsuario")
-	header.classList.toggle("active")
-})
